0444f52ed716b40e17aed53cf60b832334ba0c06
ice <jefersonharisson15@gmail.com>
2023-10-29 19:35:44 -0300

    teste

--- processar_dados.php
+++ processar_dados.php
@@ -1,26 +1,25 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    // $dbHost =  'sql10.freesqldatabase.com';
-    // $dbUsername = 'sql10657416';
-    // $dbPassword = 'pxybUc2wt8';
-    // $dbName = 'sql10657416';
+    $dbHost =  'sql10.freesqldatabase.com';
+    $dbUsername = 'sql10657416';
+    $dbPassword = 'pxybUc2wt8';
+    $dbName = 'sql10657416';
 
 
-    // $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-    // if($conexao->connect_errno)
-    // {
-    //     echo "Erro";
-    // }
-    // else
-    // {
-    //     echo "Conexão efetuada com sucesso";
-    // }
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    if($conexao->connect_errno)
+    {
+        echo "Erro";
+    }
+    else
+    {
+        echo "Conexão efetuada com sucesso";
+    }
 
-    // $email = $_POST['email'];
-    // $senha = $_POST['senha'];
-    // $result = mysqli_query($conexao, "INSERT INTO usuarios(email,senha) VALUES ('$email','$senha')");
-    // header('location: index.html');
-    var_dump($_POST);
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $result = mysqli_query($conexao, "INSERT INTO usuarios(email,senha) VALUES ('$email','$senha')");
+    header('location: index.html');
 }
 ?>
